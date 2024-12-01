ea85949ff790b0356e0b4b60c085f4de264ef223
parasdiwakar1 <paras208017@gmail.com>
2024-12-01 12:22:30 +0530

    all code

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,17 +3,22 @@ import { FaLinkedin, FaTwitter, FaFacebookF, FaInstagram } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <footer className="bg-[#123557] text-white py-6"> 
-        <hr className="border-t border-white w-[75.25rem] mx-auto" /> 
-      <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
-    
-        <div className="mb-6 md:mb-0">
-          <img className="w-60 h-30" src="https://s3-alpha-sig.figma.com/img/c1e1/45c4/467f6c3f60c323a607803f6d0dbc78cd?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LwGNvP9grakp8xukjFGzQ8e4bX0AtkaicxS8OEqaWy35bcko921VRolDehVE95Zn-5MZMVIUvhO4cFu7F~CHafsaR3Y~4y8J7pPLpVep4U49YqynFwI1JOPuUBp~obXaCbEaANwEF3dNPWEGFsbwqcSAeWoHwY14MW~qT5GdlK6JlMfR7-~G7bXdGSNT5tuQAk5WdYjNvXk5PAVvBDh8dkRWH9Mgkvb8hxAiAzNh9kKP5WZhSmhthIF1BYUEaL3-kE1M-LhfPs-orVzvAAB0jpKgGdTXUGLu41ft1an~JA8dBRin7cGQnwSX-u3dzVvK8-i~xWQLIPLa0nlh3gUo-A__" alt="" />
-          <p className="text-sm mt-1">© R Singhania</p>
+    <footer className="bg-[#123557] text-white py-6">
+      <hr className="border-t border-white w-[90%] mx-auto" />
+      <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+        
+        {/* Left Section */}
+        <div className="text-center md:text-left">
+          <img
+            className="w-40 h-auto mx-auto md:mx-0"
+            src="https://s3-alpha-sig.figma.com/img/c1e1/45c4/467f6c3f60c323a607803f6d0dbc78cd?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LwGNvP9grakp8xukjFGzQ8e4bX0AtkaicxS8OEqaWy35bcko921VRolDehVE95Zn-5MZMVIUvhO4cFu7F~CHafsaR3Y~4y8J7pPLpVep4U49YqynFwI1JOPuUBp~obXaCbEaANwEF3dNPWEGFsbwqcSAeWoHwY14MW~qT5GdlK6JlMfR7-~G7bXdGSNT5tuQAk5WdYjNvXk5PAVvBDh8dkRWH9Mgkvb8hxAiAzNh9kKP5WZhSmhthIF1BYUEaL3-kE1M-LhfPs-orVzvAAB0jpKgGdTXUGLu41ft1an~JA8dBRin7cGQnwSX-u3dzVvK8-i~xWQLIPLa0nlh3gUo-A__"
+            alt="Logo"
+          />
+          <p className="text-sm mt-2">© R Singhania</p>
         </div>
 
-
-        <div className="grid grid-cols-3 gap-8 text-sm">
+        {/* Middle Section */}
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 text-sm text-center md:text-left">
           <div>
             <h3 className="font-semibold mb-2">Company</h3>
             <ul>
@@ -39,7 +44,7 @@ const Footer = () => {
         </div>
 
         {/* Right Section */}
-        <div className="flex space-x-4 text-xl mt-6 md:mt-0">
+        <div className="flex justify-center md:justify-end space-x-4 text-xl">
           <a href="#" className="hover:text-gray-300">
             <FaLinkedin />
           </a>
@@ -54,7 +59,7 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      <hr className="border-t border-white w-[75.25rem] mx-auto" /> {/* Horizontal line with custom width of 49.25rem */}
+      <hr className="border-t border-white w-[90%] mx-auto mt-4" />
     </footer>
   );
 };
